848cbc579dcf78b207033e305f2adc62c5fb1312
Samanta <sasy24@student.bth.se>
2025-11-07 11:20:52 +0100

    adding a third vertical tile, colorchange

--- project/project.ino
+++ project/project.ino
@@ -17,22 +17,22 @@ LilyGo_Class amoled;
 static lv_obj_t* tileview;
 static lv_obj_t* t1;
 static lv_obj_t* t2;
+static lv_obj_t* t3;
 static lv_obj_t* t1_label;
 static lv_obj_t* t2_label;
+static lv_obj_t* t3_label;
 static bool t2_dark = false;  // start tile #2 in light mode
 
 //API
 HTTPClient http;
 static const String API_URL = " ";
 
-
-
 // Function: Tile #2 Color change
 static void apply_tile_colors(lv_obj_t* tile, lv_obj_t* label, bool dark)
 {
   // Background
   lv_obj_set_style_bg_opa(tile, LV_OPA_COVER, 0);
-  lv_obj_set_style_bg_color(tile, dark ? lv_color_black() : lv_color_white(), 0);
+  lv_obj_set_style_bg_color(tile, dark ? lv_color_hex(0x34429E) : lv_color_hex(0xB1DFF2), 0);
 
   // Text
   lv_obj_set_style_text_color(label, dark ? lv_color_white() : lv_color_black(), 0);
@@ -47,20 +47,23 @@ static void on_tile2_clicked(lv_event_t* e)
 
 // Function: Creates UI
 static void create_ui()
-{
+{ 
   // Fullscreen Tileview
   tileview = lv_tileview_create(lv_scr_act());
   lv_obj_set_size(tileview, lv_disp_get_hor_res(NULL), lv_disp_get_ver_res(NULL));
   lv_obj_set_scrollbar_mode(tileview, LV_SCROLLBAR_MODE_OFF);
 
   // Add two horizontal tiles
-  t1 = lv_tileview_add_tile(tileview, 0, 0, LV_DIR_HOR);
+  t1 = lv_tileview_add_tile(tileview, 0, 0, LV_DIR_HOR | LV_DIR_VER);
   t2 = lv_tileview_add_tile(tileview, 1, 0, LV_DIR_HOR);
+  //vertical tile 
+  t3 = lv_tileview_add_tile(tileview, 0, 1, LV_DIR_VER);
+
 
   // Tile #1
   {
     t1_label = lv_label_create(t1);
-    lv_label_set_text(t1_label, "Hello Students");
+    lv_label_set_text(t1_label, "Weather forecast data");
     lv_obj_set_style_text_font(t1_label, &lv_font_montserrat_28, 0);
     lv_obj_center(t1_label);
     apply_tile_colors(t1, t1_label, /*dark=*/false);
@@ -69,16 +72,25 @@ static void create_ui()
   // Tile #2
   {
     t2_label = lv_label_create(t2);
-    lv_label_set_text(t2_label, "Welcome to the workshop");
+    lv_label_set_text(t2_label, "Historical weather data / graph");
     lv_obj_set_style_text_font(t2_label, &lv_font_montserrat_28, 0);
     lv_obj_center(t2_label);
 
     apply_tile_colors(t2, t2_label, /*dark=*/false);
     lv_obj_add_flag(t2, LV_OBJ_FLAG_CLICKABLE);
     lv_obj_add_event_cb(t2, on_tile2_clicked, LV_EVENT_CLICKED, NULL);
-    
+  }
+
+  // Tile #3
+  {
+    t3_label = lv_label_create(t3);
+    lv_label_set_text(t3_label, "third tile?");
+    lv_obj_set_style_text_font(t3_label, &lv_font_montserrat_28, 0);
+    lv_obj_center(t3_label);
+    apply_tile_colors(t3, t3_label, /*dark=*/false);
   }
 }
+
 
 // Function: Connects to WIFI
 static void connect_wifi()
